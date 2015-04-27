018eff8a69e355669ef4f22d385b62cd964cf084
David Cuadra Quevedo <dcuadraq@gmail.com>
2015-04-27 14:45:38 -0500

    Removed commented code, added image for selecting with Os.

--- game.rb
+++ game.rb
@@ -23,64 +23,9 @@ class TicTacToe < Gosu::Window
     @tic_tac_doh.add_player(nickname: 'Player 2', mark: 'o')
   end
 
-  # def draw_grip
-  #   cell_number = 0
-  #   @tic_tac_doh.grip.each do |row|
-  #     row.each do |cell|
-  #       unless cell is_a? Numeric
-  #         draw_cell cell_number cell
-  #       end
-  #       cell_number += 1
-  #     end
-  #   end
-  # end
-
-  # def draw_cell(cell_number, mark)
-  #   row = 0
-  #     for n in 0..cell_number
-  #       row += 1 if (n % grid.size) == 0 unless n == 0
-  #     end
-  #     column = cell_number - (row * grid.size)
-  #     if mark == 'x'
-
-  #     {row: row, column: column}
-  # end
-
-  # def print_grid
-  #   max_digits = (@game.grid.size * @game.grid.size - 1).to_s.size
-  #   @game.grid.each do |row|
-  #     line = []
-  #     row.each do |cell|
-  #       if cell.is_a? Numeric
-  #         line << cell.to_s.rjust(max_digits, '0')
-  #       else
-  #         line << cell.to_s.center(max_digits)
-  #       end
-  #     end
-  #     puts line.join('|')
-  #   end
-  # end
-
-  # def play
-  #   match
-  #   puts "#{@game.winner[:nickname]} won"
-  #   puts "Another match? no to quit"
-  #   @game.reset
-  # end
-
-  # def match
-  #   until @game.game_over?
-  #     puts "#{@game.who_is_next[:nickname]}'s turn"
-  #     next_turn
-  #   end
-  # end
-
-  # def next_turn
-  #   @game.next_turn(0)
-  # end
-
   def cursors
-    @x = Gosu::Image.new(self, "x.png", true)
+    image = "#{@tic_tac_doh.who_is_next[:mark]}.png"
+    @x = Gosu::Image.new(self, image, true)
     @x_x = 10
     @x_y = 15
   end
@@ -96,12 +41,11 @@ class TicTacToe < Gosu::Window
     if @tic_tac_doh.next_turn(position)
       @xs << { image: Gosu::Image.new(self, "x.png", true), x: @x_x, y: @x_y } unless @tic_tac_doh.who_is_next[:mark] == 'x'
       @os << { image: Gosu::Image.new(self, "o.png", true), x: @x_x, y: @x_y } unless @tic_tac_doh.who_is_next[:mark] == 'o'
+      cursors
     end
   end
 
   def position
-    # 10  155 300
-    # 15  160 305
     col = case @x_x
           when 10
             0
@@ -122,7 +66,7 @@ class TicTacToe < Gosu::Window
   end
 
   def game_over_message
-    message = @tic_tac_doh.winner ? "#{@tic_tac_doh.winner[:nickname]} won" : "Draw"
+    message = @tic_tac_doh.winner ? "#{@tic_tac_doh.winner[:nickname]} won" : "Losers!"
     @image = Gosu::Image.from_text(self, message, Gosu.default_font_name, 100)
   end
 
